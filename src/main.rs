253c74567b0a7060132f573a185882898b3cc368
Amund Raniseth <amund.raniseth@gmail.com>
2022-12-12 13:21:23 +0100

    working hard but no worky :(

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@
 //mod day2;
 //mod day3;
 //mod day4;
-mod day5;
+//mod day5;
+//mod day6;
+//mod day7;
+//mod day8;
+mod day9;
+//mod day10;
+//mod day11;
 
 fn main() {
     //println!("Hello from main!");
@@ -10,5 +16,9 @@ fn main() {
     //day2::day2();
     //day3::day3();
     //day4::day4();
-    day5::day5();
+    //day5::day5();
+    //day6::day6();
+    //day7::day7();
+    //day8::day8();
+    day9::day9();
 }
